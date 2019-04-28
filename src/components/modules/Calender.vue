1593142f7876b6ae94cd148538b2526b350b27bc
ujiuji54 <r114306@g.kobe-kosen.ac.jp>
2019-04-29 01:22:56 +0900

    カレンダーコンポーネント

---
+++ src/components/modules/Calender.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="calender">
+    <h3>{{ yearMonth }}</h3>
+    <p>
+      <table>
+        <tr>
+          <th v-for="date in weekList" :key="date">{{ date }}</th>
+        </tr>
+        <tr v-for="week in calenderData" :key="week">
+          <td v-for="day in week" :key="day">{{ day }}</td>
+        </tr>
+      </table>
+    </p>
+  </div>
+</template>
+
+<script>
+import moment from 'moment'
+
+export default {
+  name: 'Calender',
+  data() {
+    return {
+      current: 0,
+      weekList: ['日', '月', '火', '水', '木', '金', '土']
+    }
+  },
+  computed: {
+    currentMoment() {
+      return moment().add(this.current, 'months')
+    },
+    yearMonth() {
+      return this.currentMoment.format('YYYY MM')
+    },
+    calenderData() {
+      // この月に何日まであるかを算出
+      const numOfMonth = this.currentMoment.endOf('month').date()
+      // この月の1日〜最終日までの配列
+      const daysOfMonth = [...Array(numOfMonth).keys()].map(i => ++i)
+      // 1日の曜日（0~6の数値で取得）
+      const firstWeekDay = this.currentMoment.startOf('month').weekday()
+      // 週ごとの二次元配列を生成
+      const data = [...Array(6)].map((empty, weekIndex) =>
+        [...Array(7)].map((empty, dayIndex) => {
+          const i = 7 * weekIndex + dayIndex - firstWeekDay
+          if (i < 0 || daysOfMonth[i] === undefined) {
+            return null
+          }
+          return daysOfMonth[i]
+        })
+      )
+      // 全てnullの配列があれば除去する
+      return data.filter(week => week.filter(day => day != null).length > 0)
+    }
+  }
+}
+</script>
