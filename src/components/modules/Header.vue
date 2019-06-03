78bb102e05c75dd5ad0794660a09dffdcf9fca3b
chelsea0108 <r114330@g.kobe-kosen.ac.jp>
2019-06-04 00:31:08 +0900

    ヘッダーカラー変更

--- src/components/modules/Header.vue
+++ src/components/modules/Header.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="header">
-    <img alt="Vue logo" src="../../assets/logo.png">
     <h2> Advent Calender </h2>
+    <ul class="main_menu">
+      <li><a href="">サインアップ</a></li>
+      <li><a href="">ログイン</a></li>
+    </ul>
   </div>
 </template>
 
@@ -10,3 +13,25 @@ export default {
   name: 'Header'
 }
 </script>
+
+<style scoped>
+
+.header {
+  background-color: skyblue;
+  display: flex;
+  align-items: center;
+  justify-content:space-around;
+}
+
+.main_menu {
+  list-style: none;
+  width:50%;
+  display: flex;
+  justify-content: space-around;
+}
+
+.main_menu li a {
+	text-decoration: none;
+	color: black;
+}
+</style>
