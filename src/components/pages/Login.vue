f706e07f3f4fadeb541ea8cda60a98b03f77a055
ujiuji54 <r114306@g.kobe-kosen.ac.jp>
2019-08-05 22:45:10 +0900

    login form 実装

--- src/components/pages/Login.vue
+++ src/components/pages/Login.vue
@@ -1,6 +1,18 @@
 <template>
   <div class="Login">
     <Header/>
+    <div class="container">
+      <label for="id"><b>id</b></label>
+      <input type="text" placeholder="Enter id" name="uname" required>
+
+      <label for="psw"><b>Password</b></label>
+      <input type="password" placeholder="Enter Password" name="psw" required>
+
+      <button type="submit">Login</button>
+      <label>
+        <input type="checkbox" checked="checked" name="remember"> Remember me
+      </label>
+    </div>
   </div>
 </template>
 
@@ -17,4 +29,19 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+  input[type=text], input[type=password] {
+    width: 100%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    display: inline-block;
+    border: 1px solid #ccc;
+    box-sizing: border-box;
+  }
+
+  .container{
+    width: 400px;
+    height: 200px;
+    padding: 16px;
+    margin: auto;
+  }
 </style>
